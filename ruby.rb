10c715e23817156f2f30c18cefed490042cae811
MohamedCK <abdulhakimm280@gmail.com>
2023-01-01 18:17:16 +0200

    Learn ruby

---
+++ ruby.rb
@@ -0,0 +1,29 @@
+puts 2+2 #INT
+puts 10/3 #INT
+puts 10/3.0 #FLOATING NUMBER
+puts "Happy" + "coding" #String
+puts "Happy" + "\n" + "coding" #String
+print "Happy" + "\n" + "coding" + "\n"
+
+
+a = 3 
+a 
+a = "Hello"
+a 
+b = "World!"
+
+puts (a + b)
+
+
+
+def hello(name)
+    puts("Hello #{name}!")
+end
+
+hello("David")
+
+def add(a, b)
+ a + b 
+end
+
+puts add(5,7)
